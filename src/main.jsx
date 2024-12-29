cb833c963ce414d4afb23a9721f06ec45df17ced
unknown <sharan.akash2511@gmail.com>
2024-12-29 21:39:29 +0530

    Revert "nothing to add"
    This reverts commit 591142fc10719f9c80202c0b1c4598b12c00376b.

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { RecoilRoot } from "recoil";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecoilRoot>
-      {"hello there "}
+      {" "}
       <App />
     </RecoilRoot>
   </React.StrictMode>
